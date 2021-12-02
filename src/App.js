4237ae223a7b1e72f98a4ca8c4b8c24f86ce1a21
Jimmy Barron <jimmy.r.barron@gmail.com>
2021-12-01 20:33:15 -0800

    4 digits working!

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { React, useState, useEffect } from 'react'
 import useTimer from 'easytimer-react-hook'
 import './App.css';
 import Countdown from './Countdown'
+import './Sliders.css'
 import SliderContainer from './SliderContainer';
 import Button from './Button';
 
@@ -10,10 +11,14 @@ function App() {
     // Used to display the current drag number for feedback. Without this the countdown is continously triggered.
     const [dragNumMinOnes, setDragNumMinOnes] = useState(0)
     const [dragNumMinTens, setDragNumMinTens] = useState(0)
+    const [dragNumSecOnes, setDragNumSecOnes] = useState(0)
+    const [dragNumSecTens, setDragNumSecTens] = useState(0)
 
     // Used to send the current drag number to the countdown on drag end event
     const [countdownMinOnes, setCountdownMinOnes] = useState(0)
     const [countdownMinTens, setCountdownMinTens] = useState(0)
+    const [countdownSecOnes, setCountdownSecOnes] = useState(0)
+    const [countdownSecTens, setCountdownSecTens] = useState(0)
     
     const [timer, isTargetAchieved] = useTimer({
         countdown: true,
@@ -22,12 +27,14 @@ function App() {
     const handleReset = () => {
         setCountdownMinOnes(0)
         setCountdownMinTens(0)
+        setCountdownSecOnes(0)
+        setCountdownSecTens(0)
         timer.stop()
     }
 
-    const countdownZeroPadder = (position) => {
+    const countdownZeroPadder = (position, set) => {
         // convert number to string then array
-        let time = timer.getTimeValues().minutes.toString().split('')
+        let time = timer.getTimeValues()[set].toString().split('')
         // if the time is a single digit; add a zero for padding and return
         if (time.length === 1) {
             time.unshift(0)
@@ -37,14 +44,16 @@ function App() {
 
     // When the user lifts their finger and a new countdown is set
     useEffect(() => {
-        let newTime = [countdownMinTens, countdownMinOnes].join('')
-        console.log('New Time:', newTime, 'Minutes');
+        let minutes = [countdownMinTens, countdownMinOnes].join('')
+        let seconds = [countdownSecTens, countdownSecOnes].join('')
+        console.log([0, 0, minutes, seconds, 0]);
         // Create an async function that starts a timer; awaits a delay before setting the time; then clears that timeout
         timer.stop()
         timer.start({
             startValues: {
-                minutes: newTime,
-            },
+                minutes: minutes,
+                seconds: seconds
+            }
         })
         // setTimeout(() => {
         //     console.log('Timer Delay Started');
@@ -55,13 +64,13 @@ function App() {
         //     })
         // }, 1000) 
 
-    }, [countdownMinTens, countdownMinOnes, timer])
+    }, [countdownMinTens, countdownMinOnes, countdownSecTens, countdownSecOnes, timer])
 
     return (
         <div className="App">
 
             <div>
-                <div className='sliders' style={{ display: 'flex' , marginLeft: '3%'}}>
+                <div className='sliders'>
     
                     <SliderContainer
                         id="sliderMinTens"
@@ -70,10 +79,10 @@ function App() {
                         dragNum={dragNumMinTens}
                         onDrag={setDragNumMinTens}
                         onDragEnd={setCountdownMinTens}
-                        displayNum={countdownZeroPadder(0)}
+                        displayNum={countdownZeroPadder(0, 'minutes')}
                         />
     
-                    <div style={{ width: '8px', }}>
+                    <div style={{ width: '16px', }}>
                     </div>
     
                     <SliderContainer
@@ -83,21 +92,49 @@ function App() {
                         dragNum={dragNumMinOnes}
                         onDrag={setDragNumMinOnes}
                         onDragEnd={setCountdownMinOnes}
-                        displayNum={countdownZeroPadder(1)}
+                        displayNum={countdownZeroPadder(1, 'minutes')}
+                    />
+
+                    <div style={{color: 'white', fontSize:'64px', margin: '0 12px'}}>:</div>
+
+                    <SliderContainer
+                        id="sliderMinTens"
+                        width="70px"
+                        maxNum="5"
+                        dragNum={dragNumSecTens}
+                        onDrag={setDragNumSecTens}
+                        onDragEnd={setCountdownSecTens}
+                        displayNum={countdownZeroPadder(0, 'seconds')}
+                        />
+    
+                    <div style={{ width: '16px', }}>
+                    </div>
+    
+                    <SliderContainer
+                        id="sliderMinOnes"
+                        width="70px"
+                        maxNum="9"
+                        dragNum={dragNumSecOnes}
+                        onDrag={setDragNumSecOnes}
+                        onDragEnd={setCountdownSecOnes}
+                        displayNum={countdownZeroPadder(1, 'seconds')}
                     />
     
                 </div>
                 
-                <Countdown
+                {/* <Countdown
                     minutes={timer.getTimeValues().minutes.toString()}
                     seconds={timer.getTimeValues().seconds.toString()}
-                />
+                /> */}
 
             </div>
 
             <Button
-                style={{ marginTop: '30px' }} onClick={handleReset}
-            />
+                style={{ marginTop: '30px' }}
+                onClick={handleReset}
+            >
+            Reset
+            </Button>
 
         </div>
     );  
--- src/Button.js
+++ src/Button.js
@@ -4,7 +4,7 @@ const Button = props => {
 
     return (
         <button onClick={props.onClick} className="Button">
-            Reset
+            {props.children}
         </button>
     )
 
