0905ecad1392174b0e5658a2e3839a3c2f9419a8
Jimmy Barron <jimmy.r.barron@gmail.com>
2021-12-13 19:04:02 -0800

    Progress bar working

--- src/App.js
+++ src/App.js
@@ -1,182 +1,200 @@
-import { React, useState, useEffect } from 'react'
-import useTimer from 'easytimer-react-hook'
-import './App.css';
-import './Sliders.css'
-import SliderContainer from './SliderContainer';
-import Button from './Button';
+import { React, useState, useEffect } from "react";
+import { motion } from "framer-motion";
+import useTimer from "easytimer-react-hook";
+import "./App.css";
+import "./Sliders.css";
+import SliderContainer from "./SliderContainer";
+import Button from "./Button";
 
 function App() {
-    
-    // COUNTDOWN MEMORY: Used to send the current drag number to the countdown on drag end event
-    const [countdownMinOnes, setCountdownMinOnes] = useState(0)
-    const [countdownMinTens, setCountdownMinTens] = useState(0)
-    const [countdownSecOnes, setCountdownSecOnes] = useState(0)
-    const [countdownSecTens, setCountdownSecTens] = useState(0)
-    
-    // const [triggerReset, setTriggerReset] = useState(0)
-    const [mode, setMode] = useState('zero')
-    
+  // COUNTDOWN MEMORY: Used to send the current drag number to the countdown on drag end event
+  const [countdownMinOnes, setCountdownMinOnes] = useState(0);
+  const [countdownMinTens, setCountdownMinTens] = useState(0);
+  const [countdownSecOnes, setCountdownSecOnes] = useState(0);
+  const [countdownSecTens, setCountdownSecTens] = useState(0);
 
-    // COUNTDOWN TIMER
-    const [countdownTimer, isCountdownDone] = useTimer({
-        countdown: true,
-        updateWhenTargetAchieved: true,
-    })
+  // const [triggerReset, setTriggerReset] = useState(0)
+  const [mode, setMode] = useState("zero");
 
-    // DELAY TIMER
-    const [delayTimer, isDelayTimerDone] = useTimer({
-        countdown: true,
-        updateWhenTargetAchieved: true,
-    })
+  // COUNTDOWN TIMER
+  const [countdownTimer, isCountdownDone] = useTimer({
+    countdown: true,
+    updateWhenTargetAchieved: true,
+  });
 
-    // NUMBER PADDER HELPER: This adds the zero padding eg. '01' instead of default of '1' so the controls show zeros instead of being blank
-    const countdownZeroPadder = (position, set) => {
-        // convert number to string then array
-        let time = countdownTimer.getTimeValues()[set].toString().split('')
-        // if the time is a single digit; add a zero for padding and return
-        if (time.length === 1) {
-            time.unshift(0)
-        } 
-        return time[position]
+  // PROGRESS METER
+  const [totalSeconds, setTotalSeconds] = useState(0)
+  const [progressMeter, setProgressMeter] = useState(0)
+
+  useEffect(() => {
+    setProgressMeter(countdownTimer.getTotalTimeValues().seconds / totalSeconds * 100)
+    console.log(progressMeter);
+  }, [countdownTimer.getTotalTimeValues().seconds])
+
+  // DELAY TIMER
+  const [delayTimer, isDelayTimerDone] = useTimer({
+    countdown: true,
+    updateWhenTargetAchieved: true,
+  });
+
+  // NUMBER PADDER HELPER: This adds the zero padding eg. '01' instead of default of '1' so the controls show zeros instead of being blank
+  const countdownZeroPadder = (position, set) => {
+    // convert number to string then array
+    let time = countdownTimer.getTimeValues()[set].toString().split("");
+    // if the time is a single digit; add a zero for padding and return
+    if (time.length === 1) {
+      time.unshift(0);
     }
+    return time[position];
+  };
 
-    // COUNTDOWN LOADER & DELAY START: Loads time values from controls into their respective countdown states; starts delay Timer
-    const loadCountdownStartDelay = (position, time) => {
-        // Clear and stop countdown
-        position === 'minTens' && setCountdownMinTens(time)
-        position === 'minOnes' && setCountdownMinOnes(time)
-        position === 'secTens' && setCountdownSecTens(time)
-        position === 'secOnes' && setCountdownSecOnes(time)
-        delayTimer.reset()
-        delayTimer.start({
-            startValues: {
-                seconds: 3,
-            }
-        })
-        setMode('delay')
+  // COUNTDOWN LOADER & DELAY START: Loads time values from controls into their respective countdown states; starts delay Timer
+  const loadCountdownStartDelay = (position, time) => {
+    // Clear and stop countdown
+    position === "minTens" && setCountdownMinTens(time);
+    position === "minOnes" && setCountdownMinOnes(time);
+    position === "secTens" && setCountdownSecTens(time);
+    position === "secOnes" && setCountdownSecOnes(time);
+    delayTimer.reset();
+    delayTimer.start({
+      startValues: {
+        seconds: 3,
+      },
+    });
+    setMode("delay");
+  };
+
+  // DELAY TIMER TO COUNT MODE
+  useEffect(() => {
+    if (isDelayTimerDone === true) {
+      setMode("count");
     }
+  }, [isDelayTimerDone]);
 
-    // DELAY TO COUNT
-    useEffect(() => {
-        if (isDelayTimerDone === true) {
-            setMode('count')
+  // RESET
+  const handleReset = () => {
+    // Clear countdown memory
+    setCountdownMinOnes(0);
+    setCountdownMinTens(0);
+    setCountdownSecOnes(0);
+    setCountdownSecTens(0);
+
+    // Clear and stop countdown
+    countdownTimer.start({
+      startValues: {
+        minutes: 0,
+        seconds: 0,
+      },
+    });
+    countdownTimer.stop();
+
+    // Reset Delay and Stop
+    delayTimer.reset();
+    delayTimer.stop();
+  };
+
+  // TIMER CONTROLS ON MODE CHANGE
+  useEffect(() => {
+    switch (mode) {
+      case "zero":
+        handleReset();
+        break;
+      case "edit":
+        if (isDelayTimerDone) {
+          handleReset();
         }
-    }, [isDelayTimerDone])
-
-     // RESET
-     const handleReset = () => {
-
-        // Clear countdown memory
-        setCountdownMinOnes(0)
-        setCountdownMinTens(0)
-        setCountdownSecOnes(0)
-        setCountdownSecTens(0)
-        
-        // Clear and stop countdown
+        break;
+      case "delay":
+        break;
+      case "count": // Compiles the time and starts the time
+        let minutes = [countdownMinTens, countdownMinOnes].join("");
+        let seconds = [countdownSecTens, countdownSecOnes].join("");
         countdownTimer.start({
-            startValues: {
-                minutes: 0,
-                seconds: 0,
-            }
-        })
-        countdownTimer.stop()
-        
-        // Reset Delay and Stop
-        delayTimer.reset()
-        delayTimer.stop()
+          startValues: {
+            minutes: minutes,
+            seconds: seconds,
+          },
+        });
+        setTotalSeconds(countdownTimer.getTotalTimeValues().seconds)
+        break;
+      default:
+        break;
     }
+  }, [mode]);
 
-    // TIMER CONTROLS ON MODE CHANGE
-    useEffect(() => {
-        switch (mode) {
-            case 'zero':
-                handleReset()
-                break
-            case 'edit':
-                if (isDelayTimerDone) {
-                    handleReset()
-                }
-                break
-            case 'delay':
-                break
-            case 'count': // Compiles the time and starts the time
-                let minutes = [countdownMinTens, countdownMinOnes].join('')
-                let seconds = [countdownSecTens, countdownSecOnes].join('')
-                countdownTimer.start({
-                    startValues: {
-                        minutes: minutes,
-                        seconds: seconds,
-                    }
-                })
-                break
-            default:
-                break;
-        }
-    }, [mode])
+  const sliderContainerProps = {
+    width: "70px",
+    isDelayTimerDone: isDelayTimerDone,
+    onDragEnd: loadCountdownStartDelay,
+    mode,
+    setMode: setMode,
+  };
 
-    useEffect(() => {
-        console.log(mode);
-    },[mode])
+  return (
+    <div className="App">
 
-    const sliderContainerProps = {
-        width: "70px",
-        isDelayTimerDone: isDelayTimerDone,
-        onDragEnd: loadCountdownStartDelay,
-        mode,
-        setMode: setMode,
-    }
-   
+      <motion.div
+        className="progressBar"
+        animate={{
+          height: `${progressMeter}%`
+        }}
+        style={{
+          position: "absolute",
+          bottom: 0, 
+          width: "100vw",
+          height: "0",
+          backgroundColor: "#111",
+        }}
+      />
 
-    return (
-        <div className="App">
+      <div className="sliders">
+        <SliderContainer
+          {...sliderContainerProps}
+          id="minTens"
+          maxNum="5"
+          countdownTime={countdownZeroPadder(0, "minutes")}
+        />
 
-                <div className='sliders'>
-    
-                    <SliderContainer
-                        {...sliderContainerProps}
-                        id="minTens"
-                        maxNum="5"
-                        countdownTime={countdownZeroPadder(0, 'minutes')}
-                    />
-    
-                    <div style={{ width: '8px', }}>   
-                    </div>
-    
-                    <SliderContainer
-                        {...sliderContainerProps}
-                        id="minOnes"
-                        maxNum="9"
-                        countdownTime={countdownZeroPadder(1, 'minutes')}
-                    />
+        <div style={{ width: "8px" }}></div>
 
-                    <div style={{color: 'white', fontSize:'64px', margin: '0 12px'}}>:</div>
+        <SliderContainer
+          {...sliderContainerProps}
+          id="minOnes"
+          maxNum="9"
+          countdownTime={countdownZeroPadder(1, "minutes")}
+        />
 
-                    <SliderContainer
-                        {...sliderContainerProps}
-                        id="secTens"
-                        maxNum="5"
-                        countdownTime={countdownZeroPadder(0, 'seconds')}
-                    />  
-    
-                    <div style={{ width: '8px', }}>
-                    </div>
-    
-                    <SliderContainer
-                        {...sliderContainerProps}
-                        id="secOnes"
-                        maxNum="9"
-                        countdownTime={countdownZeroPadder(1, 'seconds')}
-                    />
-    
-                </div>
-
-
-            <Button style={{ marginTop: '30px' }} onClick={() => {setMode('zero')}}>
-                Reset
-            </Button>
-
+        <div style={{ color: "white", fontSize: "64px", margin: "0 12px" }}>
+          :
         </div>
-    );  
+
+        <SliderContainer
+          {...sliderContainerProps}
+          id="secTens"
+          maxNum="5"
+          countdownTime={countdownZeroPadder(0, "seconds")}
+        />
+
+        <div style={{ width: "8px" }}></div>
+
+        <SliderContainer
+          {...sliderContainerProps}
+          id="secOnes"
+          maxNum="9"
+          countdownTime={countdownZeroPadder(1, "seconds")}
+        />
+      </div>
+
+      <Button
+        style={{ marginTop: "30px" }}
+        onClick={() => {
+          setMode("zero");
+        }}
+      >
+        Reset
+      </Button>
+    </div>
+  );
 }
 
 export default App;
