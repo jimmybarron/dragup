2d066d445e7a4aa89643955a1fe4b72dcec0ee92
Jimmy Barron <jimmy.r.barron@gmail.com>
2022-02-03 13:20:14 -0800

    Const Rotation. Scale Y to progressMeter.

--- src/AnimatedBg.js
+++ src/AnimatedBg.js
@@ -1,14 +1,21 @@
-import { React, useState, useEffect } from "react";
-import { motion } from "framer-motion-3d";
-import { Canvas } from "react-three-fiber";
+import { useState, useEffect, useRef } from "react";
+// import { motion } from "framer-motion-3d";
+import { Canvas, useFrame } from "react-three-fiber";
 import "./AnimatedBg.css";
 
 const Cube = ({ progressMeter, ...props }) => {
+  const progressBox = useRef();
+
+  useFrame(({ clock }) => {
+    progressBox.current.rotation.y = clock.getElapsedTime();
+    progressBox.current.scale.y = progressMeter / 100;
+  });
+
   return (
-    <motion.mesh {...props} animate={{ rotateY: progressMeter * 0.1 }}>
+    <mesh ref={progressBox} {...props}>
       <boxBufferGeometry attach="geometry" args={[1, 3, 1]} />
       <meshStandardMaterial attach="material" color="white" />
-    </motion.mesh>
+    </mesh>
   );
 };
 
